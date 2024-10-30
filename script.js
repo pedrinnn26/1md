49e1614e2ea0126c7984f5c72498ddb40d19e935
pedrinnn26 <pedro.juliani.delfino@escola.pr.gov.br>
2024-10-30 08:49:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function mostra resultado ()
+function mostra resultado()
 {
 var número1=10;
 var número2=15;
 var resultado=número1+número2
 document.getElementById('resultado').innerHTML=resultado
+}
